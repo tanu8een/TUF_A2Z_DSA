17bb5a7f5ad52e5d4495eb91af5093c235377a6f
tanu8een <60546683+tanu8een@users.noreply.github.com>
2022-12-05 01:39:57 +0530

    Code Java switch statement in java

---
+++ 1. Learn the basics/Java_Switch_Case_Statement.java
@@ -0,0 +1,67 @@
+/*
+
+
+Given an integer choice denoting the choice of the user and a list containing the single value R or two values L and B depending on the choice.
+If the user's choice is 1, calculate the area of the circle having the given radius(R).  
+Else if choice is 2, calculate the area of the rectangle with given length(L) and breadth(B).
+
+ 
+
+Example 1:
+
+Input: 
+choice = 1, 
+R = 5
+Output: 78.53981633974483
+Explaination: The choice is 1. 
+So we have to calculate the area of the circle.
+ 
+
+Example 2:
+
+Input: 
+choice = 2, 
+L = 5, B = 10
+Output: 50
+Explaination: Here we have to calculate the 
+area of the rectangle.
+ 
+
+Your Task:
+You do not need to read input or print anything. Your task is to complete the function switchCase() which takes choice and a list arr[], containing the single value R or the two values L and B, as input parameters. It should return area of the desired geomatrical figure.
+Note: Use Math.PI for the value of pi.
+
+ 
+
+Expected Time Complexity: O(1)
+Expected Auxiliary Space: O(1)
+
+ 
+
+Constraints:
+1 ≤ R, L, B ≤ 100 
+*/
+
+
+
+/* ######################################### CODE ############################################## */
+
+
+
+// User function Template for Java
+
+class Solution{
+    static double switchCase(int choice, List<Double> arr){
+        // code here
+        double area = 0;
+        switch(choice){
+            case 1:
+                area = Math.PI*arr.get(0)*arr.get(0);
+                break;
+            case 2:
+                area = arr.get(0)*arr.get(1);
+                break;
+        }
+        return area;
+    }
+}
